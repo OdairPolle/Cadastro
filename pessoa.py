f871e883a7d4dd059e5365e09be705ebf23ca148
Odair Polle da Silva <90409744+OdairPolle@users.noreply.github.com>
2024-08-22 14:02:06 -0300

    Update pessoa.py
    add pessoa juridica

--- pessoa.py
+++ pessoa.py
@@ -51,4 +51,26 @@ class PessoaFisica(Pessoa):
         
 #CLASSE PESSOA JURIDICA
 class PessoaJuridica(Pessoa): 
-    pass
+    #Inicializar os atributos que foram herdados e proprios atributos da classe
+    def __init__(self, nome = "", rendimento=0.0, endereco=None, cnpj=""):
+        if endereco is None:
+            # Se nenhum endereco for fornecido, cria um objeto Endereco padrao
+            endereco = Endereco()
+        super().__init__(nome, rendimento, endereco)
+        self.cnpj = cnpj
+     
+        
+    def calcular_imposto(self, rendimento: float) -> float:
+        # Sem imposto para rendimentos ate 10500
+        if rendimento <= 10500:
+            return 0
+        # 2% de imposto para rendimento entre 10500 e 30500
+        elif 10500 < rendimento <= 30500:
+            return (rendimento / 100) * 5
+            # return rendimento * 0.2
+        # 3.5% de imposto para rendimentos entre 30500 e 60000   
+        elif 30500 < rendimento <= 60000:
+            return (rendimento / 100) * 8   
+        # 5% de imposto para rendimentos acima de 60000     
+        else: 
+            return (rendimento / 100) * 12   
